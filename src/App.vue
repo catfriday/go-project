39a5a941cc41fe8ee5297b5114500a34d91652e4
Catrina Friday <cat.friday@gmail.com>
2020-11-21 14:49:09 -0500

    added methods to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,7 @@
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 <template>
   <div id="app" class="container">
     <div class="row">
@@ -12,7 +16,39 @@
             <button class="btn btn-primary">Generate!</button>
           </div>
         </form>
+      <img :src="thumbnailUrl"/>
       </div>
     </div>
   </div>
 </template>
+
+ import axios from 'axios';
+<script>
+export default {
+  name: 'App',
+
+  data() { return {
+    websiteUrl: '',
+    thumbnailUrl: '',
+  } },
+
+  methods: {
+    makeWebsiteThumbnail() {
+  this.axios.post("https://screenshotapi.net/api/v1/screenshot", {
+    token: "PGMNDQCHPWVIHIHJUBXGDO3U6VKNYLOX",
+    url: this.websiteUrl,
+    width: 1920,
+    height: 1080,
+    output: 'json',
+    thumbnail_width: 300
+  })
+  .then((response) => {
+    this.thumbnailUrl = response.data.screenshot;
+  })
+  .catch((error) => {
+    window.alert(`The API returned an error: ${error}`);
+  })
+}
+  }
+}
+</script>
